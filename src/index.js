c08618de8951c1ce3f4baf018c57382cc3897cec
devmark <markguyadaez@gmail.com>
2025-04-09 19:54:44 +0800

    use express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+console.log("Hello World");
+
+import express from "express";
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+app.use("/", (req, res) => {
+  res.send("Hello World2");
+});
+
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
